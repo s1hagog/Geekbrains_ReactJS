ca6e43c8f836503dc8eb1d3465eff53fc6877e2a
Aleksandr Moshak <moshakalex@gmail.com>
2019-10-07 14:21:21 -0700

    code from lecture / react-router 5 tutorial

---
+++ lesson6/src/app/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About this application</h1>
+                <p>1Incididunt qui amet eiusmod consequat aute enim officia sunt sit excepteur excepteur mollit consequat. Pariatur cupidatat tempor voluptate labore enim commodo non esse qui. Fugiat enim anim est cillum consequat amet enim commodo elit Lorem in. Laboris duis voluptate et nulla non veniam eiusmod ad sit. Voluptate consequat id voluptate in deserunt. Reprehenderit nostrud velit aliqua nostrud cupidatat excepteur pariatur culpa ullamco ullamco laborum.</p>
+            </div>
+        )
+    }
+}
---
+++ lesson6/src/app/pages/Contacts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Contacts extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contacts</h1>
+                <p>Phone: 999-999-9999</p>
+            </div>
+        )
+    }
+}
