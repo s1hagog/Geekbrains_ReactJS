b1d76ce7607e0a4e539bb8fdaeb4be395a8a5dd8
Aleksandr Moshak <moshakalex@gmail.com>
2019-10-07 17:35:17 -0700

    lesson 6 - flux tutorial

---
+++ lesson6_2/src/components/UsersList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import User from './User';
+
+export default class UsersList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            users: []   
+        }
+    }
+    render() {
+        if(!this.state.users.length) {
+            return null
+        }
+
+        const users = this.state.users.map(user => {
+            return <User key={user.id} {...user} noLink={false}/>
+        })
+
+        return (
+            <div>
+                <h1>Users</h1>
+                {users}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/users').then(response => {
+            this.setState({users: response.data});
+        })
+    }
+}
---
+++ lesson6_2/src/layouts/Layout.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Link} from 'react-router';
+import Menu from '../components/Menu';
+import MenuItem from '../components/MenuItem';
+
+
+export default class Layout extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.brand = 'React Blog';
+
+    }
+
+    isActiveMain(href) { 
+        return window.location.pathname == href;
+    }
+
+    isActive(href) {
+        return window.location.pathname.includes(href);
+    }
+
+    render(){
+        return(
+            <>
+                <Menu brand={this.brand}>
+                    <MenuItem href="/" active={this.isActiveMain('/')}>Main</MenuItem>
+                    <MenuItem href="/users" active={this.isActive('users')}>Users</MenuItem>
+                    <MenuItem href="/posts" active={this.isActive('posts')}>Posts</MenuItem>
+                    <MenuItem href="/comments" active={this.isActive('comments')}>Comments</MenuItem>
+                </Menu>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+                <div className="card-footer">
+                    &copy; 2019
+                </div>
+            </>
+        );
+    }
+}
