08ecdab2ffa748ee30997c9fab7f1f5c67132bca
Aleksandr Moshak <moshakalex@gmail.com>
2019-10-13 16:31:20 -0700

    hw_8 changed all react classes to hooks

---
+++ lesson8_hw/src/components/MenuItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+
+// export default class MenuItem extends Component {
+//     render() {
+//         return (
+//             <li className={this.props.active ? 'nav-item active' : 'nav-item'}>
+//                 <Link className="nav-link" to={this.props.href}>
+//                     {this.props.children}
+//                 </Link>
+//             </li>
+//         )
+//     }
+// }
+
+const MenuItem = (props) => {
+    return (
+        <li className={props.active ? 'nav-item active' : 'nav-item'}>
+            <Link className="nav-link" to={props.href}>
+                {props.children}
+            </Link>
+        </li>
+    )
+}
+
+export default MenuItem;
